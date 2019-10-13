465e90b59e920c7bc882de4de82b85126346b309
Jose Silva <tuf06643@temple.edu>
2019-10-13 14:25:58 -0400

    Added Arm interface details and method outlines

---
+++ src/Arm.java
@@ -0,0 +1,58 @@
+public interface Arm{
+
+    // shoulder(), elbow(), wrist(), claw()
+
+    /**
+     * <h1>shoulder()</h1>
+     * This method is used to invoke the shoulder mechanism built into
+     * the Mega 2560 GFS robot tank's arm
+     *
+     * @author Jose Silva
+     * @version 1.0
+     * @since 2019-10-13
+     */
+    public void shoulder();
+
+
+    /**
+     * <h1>elbow()</h1>
+     * This method is used to invoke the elbow mechanism of the
+     * Mega 2560 GFS robot tank's arm
+     *
+     * @author Jose Silva
+     * @version 1.0
+     * @since 2019-10-13
+     */
+    public void elbow();
+
+
+    /**
+     * <h1>wrist()</h1>
+     * This method is used to invoke the wrist mechanism of the
+     * Mega 2560 GFS robot tank's arm
+     *
+     * @author Jose Silva
+     * @version 1.0
+     * @since 2019-10-13
+     */
+    public void wrist();
+
+
+    /**
+     * <h1>claw()</h1>
+     * This method is used to invoke the claw mechanism of the
+     * Mega 2560 GFS robot tank's arm.
+     *
+     * @author Jose Silva
+     * @version 1.0
+     * @since 2019-10-13
+     */
+    public void claw();
+
+
+
+
+
+
+
+}
