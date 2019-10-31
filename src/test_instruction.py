b52cda4f98987f2555d80b611ab36dbf0cd4e41a
Sean DiGirolamo <tug70176@temple.edu>
2019-10-31 11:45:21 -0400

    finished intruction tests. Unfortunately, unit testing crashes for some arcane python reason. Yay! Isn't python the best?

--- src/test_instruction.py
+++ src/test_instruction.py
@@ -8,23 +8,34 @@ IMG = 'temporary'
 TREADS = [{"angle": 0.0, "distance": 1.1}]
 ARMS = [{"angle": 2.2}]
 
+
 class TestInstruction(TestCase):
     def __init__(self):
         super().__init__()
         self.json_obj = Instruction(JSON_STRING)
-        self.data_obj = Instruction()
+        self.data_obj = Instruction(STATUS, IMG, TREADS, ARMS)
 
     def test_json(self):
+        assert self.json_obj.json() == JSON_STRING
+        assert self.data_obj.json() == JSON_STRING
         self.fail()
 
     def test_status(self):
+        assert self.json_obj.status() == STATUS
+        assert self.data_obj.status() == STATUS
         self.fail()
 
     def test_img(self):
+        assert self.json_obj.img() == IMG
+        assert self.data_obj.img() == IMG
         self.fail()
 
     def test_treads(self):
+        assert self.json_obj.treads() == TREADS
+        assert self.data_obj.treads() == TREADS
         self.fail()
 
     def test_arms(self):
+        assert self.json_obj.arms() == ARMS
+        assert self.data_obj.arms() == ARMS
         self.fail()
