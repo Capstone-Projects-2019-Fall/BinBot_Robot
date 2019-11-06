3141238b34f2a9dff731cfde463894e610a7bb0e
Sean DiGirolamo <tug70176@temple.edu>
2019-11-06 14:45:33 -0500

    Removing print statements

--- src/interfaces/Connection.py
+++ src/interfaces/Connection.py
@@ -19,42 +19,28 @@ class Connection:
         self.__sock.connect((ip, port))
 
     def send(self, msg):
-        print("SENDING MESSAGE " + msg)
         self.__sock.sendall(str.encode(msg + "\n"))
         self.__receive_ack()
         print()
 
     def receive(self):
-        print("RECEIVING")
-
         length = self.__receive_length()
-
         retval = self.__sock.recv(length).decode("utf-8")
-        print("RECEIVED " + retval)
-
         self.__send_ack()
         self.__drain()
-
-        print()
-
         return retval
 
     def __receive_length(self):
-        print("Waiting for length")
         retval = int.from_bytes(self.__sock.recv(JAVA_INT_BYTES), ENDIAN)
-        print("Received length: " + str(retval))
         self.__send_ack()
         return retval
 
     def __send_ack(self):
-        print("Sending ACK")
         self.__sock.sendall(ACK)
         self.__sock.sendall(str.encode("\n"))
 
     def __receive_ack(self):
-        print("Waiting for ACK")
         data = self.__sock.recv(ACK_BYTES)
-        print("Received ACK: " + str(data))
 
     def __drain(self):
         """remove the data present on the socket"""
