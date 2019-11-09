f8486bd5207ed54e0ef577a97c41e7d690b6b3a5
Jose Silva <tuf06643@temple.edu>
2019-11-09 13:07:45 -0500

    implemented camera_ang method

--- src/interfaces/Camera.py
+++ src/interfaces/Camera.py
@@ -1,3 +1,37 @@
 # Author: Jose Silva
 # Date Created: November 9th, 2019
+# File Name: Camera.py
+# Description: Camera functionality
 
+
+from __future__ import division
+import time
+import RPi.GPIO as GPIO
+import Adafruit_PCA9685
+
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(50)
+
+
+org_pos = L11_ST1
+
+def camera_ang(direction, ang): # Camera angle function
+	global org_pos
+	if ang == 0:
+		ang=4
+	if direction == 'lookdown':
+		if org_pos <= L11_MAX:
+			org_pos+=ang
+		else:
+			org_pos = L11_MAX
+	elif direction == 'lookup':
+		if org_pos >= L11_MIN:
+			org_pos-=ang
+		else:
+			org_pos = L11_MIN
+	elif direction == 'home':
+		org_pos = L11_MAX
+	else:
+		pass
+	#print(ang)
+	pwm.set_pwm(11,0,org_pos)
