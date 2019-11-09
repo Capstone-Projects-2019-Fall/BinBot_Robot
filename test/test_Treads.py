61f90dda5a47c38c6d4002656869edc3da8748ee
Sean <SeanR1194@gmail.com>
2019-11-08 21:49:02 -0500

    unit test for Treads

---
+++ test/test_Treads.py
@@ -0,0 +1,40 @@
+import json
+import unittest
+from src.interfaces import Treads
+
+forward = {"angle": 0,
+           "distance": 1.5
+           }
+
+backward = {"angle": 180,
+            "distance": 1.5
+            }
+
+left = {"angle": 270,
+        "distance": 0.0
+        }
+
+right = {"angle": 90,
+         "distance": 0.0
+         }
+
+instructions = dict(treads=[forward, left, right])
+
+# converts python data structure to JSON string
+json_str = json.dumps(instructions)
+
+# converts JSON string into python data structure
+json_obj = json.loads(json_str)
+
+
+class TestInstruction(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_execute(self):
+        Treads.execute(instructions)
+
+
+if __name__ == '__main__':
+    unittest.main()
