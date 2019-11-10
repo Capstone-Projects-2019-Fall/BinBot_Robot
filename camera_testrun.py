bf875391a1d495f4ead2dcaa05933c5279e07285
Jose Silva <tuf06643@temple.edu>
2019-11-10 13:37:03 -0500

    Changed camera_testrun.py for testing purposes

--- camera_testrun.py
+++ camera_testrun.py
@@ -1,6 +1,7 @@
-from src.interfaces.Camera import Camera
+import picamera
 
-print ("About to take a photo using pi camera")
-
-c = Camera()
-c.take_photo()
+print("About to take a picture")
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)
+    camera.capture("/home/pi/Desktop/newImage.jpg")
+print("Picture was taken with pi camera")
