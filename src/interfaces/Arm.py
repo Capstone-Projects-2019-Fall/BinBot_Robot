c5076fa9e86a01a53138dbc732e1b5df7cc54f3c
Jose Silva <tuf06643@temple.edu>
2019-11-14 13:37:36 -0500

    Created claw movement method to rotate claw

--- src/interfaces/Arm.py
+++ src/interfaces/Arm.py
@@ -16,7 +16,7 @@ pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
 
-def hand(command): ## Control the arm movements in and out
+def hand(command):  # Control the arm movements in and out
     if command == 'in':
         pwm.set_pwm(13, 0, L13_ST3)
         pwm.set_pwm(12, 0, L12_ST4)
@@ -27,3 +27,11 @@ def hand(command): ## Control the arm movements in and out
         pwm.set_pwm(12, 0, L12_ST1)
         pwm.set_pwm(13, 0, L13_ST1)
 
+
+def hand_pos(pos):    # Believe to control the claw rotation
+    if pos <= 4:
+        pwm.set_pwm(12, 0, L12_ST1 - 30 * pos)
+        pwm.set_pwm(13, 0, L13_ST1 - 30 * pos)
+    else:
+        pwm.set_pwm(12, 0, (L12_ST1 - 24 * pos))
+        pwm.set_pwm(13, 0, L13_ST3 - 6 * (pos - 4))
