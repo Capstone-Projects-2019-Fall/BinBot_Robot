c11c965fdd3b1a5cc9e36b64bbbdc3ed9f61f839
Jose Silva <tuf06643@temple.edu>
2019-11-14 13:25:38 -0500

    Created method to control in and out movement of arm

--- src/interfaces/Arm.py
+++ src/interfaces/Arm.py
@@ -5,3 +5,25 @@ close claw / open claw
 Author: Jose Silva
 Date: 2019/11/14
 '''
+
+from __future__ import division
+import time
+import RPi.GPIO as GPIO
+import sys
+import Adafruit_PCA9685
+
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(50)
+
+
+def hand(command): ## Control the arm movements in and out
+    if command == 'in':
+        pwm.set_pwm(13, 0, L13_ST3)
+        pwm.set_pwm(12, 0, L12_ST4)
+        time.sleep(0.5)
+        pwm.set_pwm(13, 0, L13_ST2)
+        pwm.set_pwm(12, 0, L12_ST2)
+    elif command == 'out':
+        pwm.set_pwm(12, 0, L12_ST1)
+        pwm.set_pwm(13, 0, L13_ST1)
+
