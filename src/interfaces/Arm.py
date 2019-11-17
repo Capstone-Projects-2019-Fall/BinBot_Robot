772452bdf26ef050a5ff39f3a739b15b208b32db
Jose Silva <tuf06643@temple.edu>
2019-11-17 18:33:18 -0500

    Added more pwm.set_pwm calls to hand() method

--- src/interfaces/Arm.py
+++ src/interfaces/Arm.py
@@ -20,12 +20,18 @@ def hand(command):  # Control the arm movements in and out
     if command == 'in':
         pwm.set_pwm(13, 0, 298)
         pwm.set_pwm(12, 0, 120)
-        time.sleep(0.5)
+        time.sleep(3)
         pwm.set_pwm(13, 0, 227)
         pwm.set_pwm(12, 0, 131)
     elif command == 'out':
-        pwm.set_pwm(12, 0, 430)
-        pwm.set_pwm(13, 0, 429)
+        pwm.set_pwm(12, 0, 130)
+        pwm.set_pwm(13, 0, 129)
+        time.sleep(3)
+        pwm.set_pwm(12, 0, 400)
+        pwm.set_pwm(13, 0, 399)
+        time.sleep(2)
+        pwm.set_pwm(12, 0, 0)
+        pwm.set_pwm(13, 0, 85)
 
 
 def hand_pos(pos):  # Believe to control the claw rotation
@@ -63,29 +69,25 @@ def clean_all():
 if __name__ == '__main__':
     try:
 
-
+        hand('in')
         pos_input = 0
-        #OUT = 1
+        OUT = 1
         while 1:
             a = input()
 
-            if a == 1:
-                hand('in')
-            elif a == 2:
-                hand('out')
-
-         #   if OUT == 1:
-         #       if pos_input < 13:
-         #           pos_input += 1
-         #       else:
-         #           print('MAX')
-         #           OUT = 0
-         #   else:
-         #       if pos_input > 1:
-         #           pos_input -= 1
-         #       else:
-         #           print('MIN')
-         #           OUT = 1
+            if OUT == 1:
+                if pos_input < 13:
+                    pos_input += 1
+                else:
+                    hand('out')
+                    print('MAX')
+                    OUT = 0
+            else:
+                if pos_input > 1:
+                    pos_input -= 1
+                else:
+                    print('MIN')
+                    OUT = 1
             catch(pos_input)
             print(pos_input)
 
