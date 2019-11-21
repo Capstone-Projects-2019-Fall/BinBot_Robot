1b0212b1269f23da51eaef6d7dcf4a7eb07641b6
Jose Silva <tuf06643@temple.edu>
2019-11-21 14:07:03 -0500

    Added comments to DistanceSensor.py

--- src/interfaces/DistanceSensor.py
+++ src/interfaces/DistanceSensor.py
@@ -5,14 +5,18 @@
 import RPi.GPIO as GPIO
 import time
 
-Tr = 11
-Ec = 8
+Tr = 11 # Trig
+Ec = 8 # Echo
 
 def checkdistance():       #Reading distance
+
+    # Initialize the Trig and Echo
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(Tr, GPIO.OUT,initial=GPIO.LOW)
     GPIO.setup(Ec, GPIO.IN)
+
+    # Get the distance information
     GPIO.output(Tr, GPIO.HIGH)
     time.sleep(0.000015)
     GPIO.output(Tr, GPIO.LOW)
@@ -22,4 +26,4 @@ def checkdistance():       #Reading distance
     while GPIO.input(Ec):
         pass
     t2 = time.time()
-    return (t2-t1)*340/2
+    return (t2-t1)*340/2 # return the distance value
