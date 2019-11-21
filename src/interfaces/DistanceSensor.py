46fdce276e2efe530b862b4313ff2c0b1fbd36d3
Jose Silva <tuf06643@temple.edu>
2019-11-21 13:23:50 -0500

    Created DistanceSensor.py to get distance using sensor

---
+++ src/interfaces/DistanceSensor.py
@@ -0,0 +1,25 @@
+# Description : Detection distance and tracking with ultrasonic
+# Author      : Jose Silva
+# Date        : 2019/11/21
+
+import RPi.GPIO as GPIO
+import time
+
+Tr = 11
+Ec = 8
+
+def checkdistance():       #Reading distance
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(Tr, GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(Ec, GPIO.IN)
+    GPIO.output(Tr, GPIO.HIGH)
+    time.sleep(0.000015)
+    GPIO.output(Tr, GPIO.LOW)
+    while not GPIO.input(Ec):
+        pass
+    t1 = time.time()
+    while GPIO.input(Ec):
+        pass
+    t2 = time.time()
+    return (t2-t1)*340/2
