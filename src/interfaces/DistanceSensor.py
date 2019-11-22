7d08b4d0ddc35f883e90641e1197e3cfe1c26b23
Jose Silva <tuf06643@temple.edu>
2019-11-22 13:28:31 -0500

    Testing checkdistance.py to find distance using sensor

--- src/interfaces/DistanceSensor.py
+++ src/interfaces/DistanceSensor.py
@@ -5,15 +5,16 @@
 import RPi.GPIO as GPIO
 import time
 
-Tr = 11 # Trig
-Ec = 8 # Echo
+Tr = 11  # Trig
+Ec = 8  # Echo
 
-def checkdistance():       #Reading distance
+
+def checkdistance():  # Reading distance
 
     # Initialize the Trig and Echo
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(Tr, GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(Tr, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(Ec, GPIO.IN)
 
     # Get the distance information
@@ -26,4 +27,10 @@ def checkdistance():       #Reading distance
     while GPIO.input(Ec):
         pass
     t2 = time.time()
-    return (t2-t1)*340/2 # return the distance value
+    return (t2 - t1) * 340 / 2  # return the distance value
+
+
+while 1:
+    print(checkdistance())
+    time.sleep(1)
+    pass
