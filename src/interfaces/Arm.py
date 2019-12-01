f870de3d177a2e238f18d10ddcb7ae6e3fce0b3c
Jose Silva <tuf06643@temple.edu>
2019-12-01 13:34:55 -0500

    Testing arm to hold position

--- src/interfaces/Arm.py
+++ src/interfaces/Arm.py
@@ -63,7 +63,7 @@ def cir_back():  # Rotates the claw back to starting position
 def home():
     pwm.set_pwm(12, 0, 450)
     pwm.set_pwm(13, 0, 399)
-    pwm.set_pwm(13, 0, 100)
+    #pwm.set_pwm(13, 0, 100)
 
 
 def clean_all():
@@ -90,49 +90,21 @@ if __name__ == '__main__':
 
         home()
 
-        while 1:
-            if .10 < DistanceSensor.checkdistance() < .11:
-                print(DistanceSensor.checkdistance())
-                openClaw()
-                time.sleep(1)
-                hand('in')
-                time.sleep(1)
-                catch()
-                time.sleep(1)
-                hand('out')
-            else:
-                print(DistanceSensor.checkdistance())
-                print("not in range")
-            time.sleep(5)
+       # while 1:
+       #     if .10 < DistanceSensor.checkdistance() < .11:
+       #         print(DistanceSensor.checkdistance())
+       #         openClaw()
+       #         time.sleep(1)
+       #         hand('in')
+       #         time.sleep(1)
+       #         catch() # pwm.set_pwm(15, 0, 574)
+       #         time.sleep(1)
+       #         hand('out')
+       #         if
+       #     else:
+       #         print(DistanceSensor.checkdistance())
+       #         print("not in range")
+       #     time.sleep(5)
 
-
-        #cir_pos(5)
-        #time.sleep(3)
-        #cir_back()
-
-
-    #        hand('in')
-    #        pos_input = 0
-    #        OUT = 1
-    #        while 1:
-    #            a = input()
-
-    #            if OUT == 1:
-    #                if pos_input < 13:
-    #                    pos_input += 1
-    #                else:
-    #                    hand('out')
-    #                    print('MAX')
-    #                    OUT = 0
-    #            else:
-    #                if pos_input > 1:
-    #                    pos_input -= 1
-    #                else:
-    #                    print('MIN')
-    #                    OUT = 1
-    #            catch(pos_input)
-    #            print(pos_input)
-
-    #            pass
     except KeyboardInterrupt:
         clean_all()
