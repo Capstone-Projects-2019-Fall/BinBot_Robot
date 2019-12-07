ae9796d93916408521d44367fc077ffc292dc438
Sean <Seanr1194@gmail.com>
2019-12-07 17:07:45 -0500

    commenting out LED stuff for debugging

--- main_loop.py
+++ main_loop.py
@@ -1,12 +1,12 @@
 import json
 import time
-from rpi_ws281x import *
+# from rpi_ws281x import *
 from src.interfaces.Connection import Connection
 from src.instructions.instruction import Instruction
 from src.interfaces import Treads
 from src.interfaces import Arm
 from src.interfaces import Camera
-from src.interfaces import LED
+# from src.interfaces import LED
 from src.interfaces import DistanceSensor
 
 # JOSE'S HOTSPOT
@@ -22,7 +22,7 @@ SeanR_laptop = "10.108.22.58"
 
 IP = SeanR_laptop
 PORT = 7001
-LED = LED.LED()
+# LED = LED.LED()
 camera = Camera.Camera()
 
 
@@ -36,8 +36,7 @@ def is_json(myjson):
 
 try:
     while True:
-    # try:
-        LED.colorWipe(Color(0, 0, 255))  # LED BLUE
+        # LED.colorWipe(Color(0, 0, 255))  # LED BLUE
 
         # Capture picture
         cur_t0 = time.time()
@@ -66,53 +65,41 @@ try:
             treads = instr_in.treads()
             if treads is not None:
                 # print(treads)
-                try:
-                    Treads.setup()
-                    for movement in treads:
-                        LED.colorWipe(Color(255, 16, 0))  # LED RED
-                        print(f"Exe: {movement}")
-                        # RETREIVE OBJECT
-                        if movement["angle"] == 0.0 and movement["distance"] == 1.0:
-                            LED.colorWipe(Color(0, 225, 0))  # LED GREEN
-                            Treads.moveBySensor()
-                            # x = DistanceSensor.checkdistance()
-                            # print(f"dist1: {x}")
-                            # x = (x*10) - 1.0
-                            # print(f"dist2: {x}")
-                            # new_movement = {"angle": 0, "distance": x}
-                            # Treads.executeTreadInstruction(new_movement)
+                Treads.setup()
+                for movement in treads:
+                    # LED.colorWipe(Color(255, 16, 0))  # LED RED
+                    print(f"Exe: {movement}")
+                    # RETREIVE OBJECT
+                    if movement["angle"] == 0.0 and movement["distance"] == 1.0:
+                        # LED.colorWipe(Color(0, 225, 0))  # LED GREEN
+                        Treads.moveBySensor()
+                        # x = DistanceSensor.checkdistance()
+                        # print(f"dist1: {x}")
+                        # x = (x*10) - 1.0
+                        # print(f"dist2: {x}")
+                        # new_movement = {"angle": 0, "distance": x}
+                        # Treads.executeTreadInstruction(new_movement)
 
-                            print("PICK UP")
-                            Arm.pick_up()
-                        else:
-                            print("skipping")
-                            # Treads.executeTreadInstruction(movement)
+                        print("PICK UP")
+                        Arm.pick_up()
+                    else:
+                        print("skipping")
+                        # Treads.executeTreadInstruction(movement)
 
-                    # Clean up after instructions
-                    LED.colorWipe(Color(0, 0, 0))
-                    Treads.destroy()
-                    Arm.clean_all()
-
-                except Exception as e:
-                    print(f"Instruction execution exception: {e}")
-                    # Treads.destroy()
-                    # Arm.clean_all()
-                    # LED.colorWipe(Color(0, 0, 0))
-                    raise
+                # Clean up after instructions
+                # LED.colorWipe(Color(0, 0, 0))
+                Treads.destroy()
 
             else:
                 print('No treads')
 
 except KeyboardInterrupt as e:
     print(f"Keyboard Interrupt: {e}")
-    LED.colorWipe(Color(0, 0, 0))
-    Treads.destroy()
-    Arm.clean_all()
     raise
 
 except Exception as e:
     print(f"Exeption throw: {e}")
-    LED.colorWipe(Color(0, 0, 0))
+    # LED.colorWipe(Color(0, 0, 0))
     Treads.destroy()
     Arm.clean_all()
     raise
